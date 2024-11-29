1837bd623ad5f206271efce49a0a23c8f02df2e4
AnaPanamito <ana.panamito@unl.edu.ec>
2024-11-29 03:23:13 -0500

    Update Boleteria.java

--- Boleteria.java
+++ Boleteria.java
@@ -1,19 +1,6 @@
-public class Boleteria {
-    //attributes
-    int entradasDisponibles;
-
-
-
-    //get
-    public Boleteria(Cliente cliente1, int entradasDisponibles) {
-        this.entradasDisponibles = entradasDisponibles;
-    }
-
-    //operations
-    public void venderEntradas(Cliente cliente1){
-        entradasDisponibles = entradasDisponibles - cliente1.entradas;
-        System.out.print("El " + cliente1.rol + " " + cliente1.nombre + "compro " + cliente1.entradas + " entradas\nEntradas disponibles: " + entradasDisponibles);
-    }
+public class boleteria {
+    private int entradasDisponibles;
+    public void venderEntrada() {
 
     }
-
+}
