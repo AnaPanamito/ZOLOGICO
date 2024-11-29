03530dee1c7e3ceef6c78641ca18a41f4431d026
AnaPanamito <ana.panamito@unl.edu.ec>
2024-11-29 03:24:14 -0500

    Update and rename Carnivoros.java to historialSalud.java

---
+++ historialSalud.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class HistorialSalud {
+    private Date fecha;
+    private String diagnostico;
+    private String tratamiento;
+    private String observacion;
+
+    public HistorialSalud(Date fecha, String diagnostico, String tratamiento, String observacion) {
+        this.fecha = fecha;
+        this.diagnostico = diagnostico;
+        this.tratamiento = tratamiento;
+        this.observacion = observacion;
+    }
+
+    public void registrarDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+
+    public void registrarTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+}
